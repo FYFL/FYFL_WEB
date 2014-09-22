b27a2ef7cdaa7c0673d776367d408b18e4b86fca
varanasisudha <varanasisudha@gmail.com>
2014-09-22 11:29:28 -0500

    Create earn-first-badge.php

---
+++ badges/earn-first-badge.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8"> 
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"> 
+	<title>Earn First Badge</title>
+	<link rel="stylesheet" href="css/normalize.css" type="text/css">
+	<link rel="stylesheet" href="css/third_level_pages.css" type="text/css">
+	
+	<script src="js/jquery-1.8.2.min.js"></script>
+	<script src="js/jquery.scrollTo-1.4.3.1.min.js"></script>
+	<script src="js/jquery.easing.1.3.js"></script>
+	<script src="js/jquery.scrollorama.js"></script>
+	<script src="js/jquery.scrolldeck.js"></script>
+
+
+<style type="text/css">
+	#bottom{
+		left:0;
+		bottom: 20px;
+		width:80%;
+		margin: 0 10%;
+		border-top:dotted 3px #999;
+		font-color:#666;
+		font-size:16px;
+	}
+
+
+#overleft{
+	width:35%;
+	float:left;
+	margin: 50px 2% 0 13%;
+	line-height:28px;
+	
+}
+
+#overright{
+	width:35%;
+	float:left;
+	margin: 50px 13% 0 2%;
+	line-height:28px;
+}
+
+
+p{
+padding:0 13% 0 13%;
+}
+ul{
+padding:0 13% 0 13%;
+}
+
+</style>
+</head>
+<body>
+	<div class=""></div>
+	<div id="header">
+		<h1>&nbsp;<img src="images/FYFLnew.png" height="50"/></h1></a>
+		<div id="nav">
+	<a href="http://www.fyflnetwork.org/4h/badges" class="nav-button">Home</a>
+	<a href="http://www.fyflnetwork.org/4h/badges/#overview" class="nav-button">4-H Badges</a>
+	<a href="http://www.fyflnetwork.org/4h/badges#badge" class="nav-button">Overview</a>	</div>
+	</div>
+   
+	<div class="slide" id="odd">	  
+       	<div style="padding-top:50px">
+        <div class="content" id="pageHead">
+		    	
+ <h2 style="text-align:center; padding:0 13% 0 13%;">Earn Your First 4-H Badge</h2></div>
+ 	
+	<div id="geninfo">
+       	<div class="words" id="testzero">
+        <p>Are you a lifelong learner? A connected learner? Click on the badges below to learn more and earn your first 4-H 		badge!		
+	</p>
+    	</div>
+ 	<div id="overleft"> 
+ 
+       <a href="http://www.fyflnetwork.org/4h/badges/source/login.php">Life long learner</a>
+	</div>
+	<div id="overright">
+  <a href="http://www.fyflnetwork.org/4h/badges/source/login.php">Connected Learner</a>
+    </div>
+	</div>
+  
+	</div>
+	</div>
+	</div>
+  
+</body>
+</html>
