8019801178f34332653db63e88f13a94dc1c9656
Donovan Jordan <dbj0005@auburn.edu>
2014-10-08 14:45:09 -0600

    More changes.

--- badges/source/MakeBadge.php
+++
@@ -1,5 +0,0 @@
-<?php
-	require_once("./include/membersite_config.php");
-	require_once("./php/tags.php");
-	require_once("./php/MakeBadge.php");
-?>
--- badges/source/MyBadges.php
+++
@@ -1,4 +0,0 @@
-<?php
-	require_once("./include/membersite_config.php");
-	require_once("./php/MyBadges.php");
-?>
